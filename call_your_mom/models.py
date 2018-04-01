662bf4ddf5cfaea6de840319893ee48a60f67c4c
Remi Rampin <remirampin@gmail.com>
2018-04-01 13:11:47 -0400

    Create models

--- call_your_mom/models.py
+++ call_your_mom/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
-# Create your models here.
+
+class CYMUser(models.Model):
+    email = models.CharField(max_length=200, primary_key=True)
+    last_login = models.DateTimeField()
+    last_email = models.DateTimeField()
+
+
+class Task(models.Model):
+    Type = Choices(('normal', _('normal')), ('exact', _('exact')))
+    type = models.CharField(max_length=8, choices=Type)
+    user = models.ForeignKey(CYMUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    description = models.CharField(max_length=280)
+    created = models.DateTimeField()
+    due = models.DateTimeField()
+
+
+class TaskDone(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    done = models.DateTimeField()
+    recorded = models.DateTimeField()
