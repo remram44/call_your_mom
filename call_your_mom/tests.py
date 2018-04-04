4d67804750531ee60230d55660ac11f81431b7b9
Remi Rampin <remirampin@gmail.com>
2018-04-04 11:28:11 -0400

    Add test for list of timezones

--- call_your_mom/tests.py
+++ call_your_mom/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+from . import views
+
+
+class TimezonesTestCase(TestCase):
+    def test_timezone_list(self):
+        # Check reasonable number
+        self.assertGreater(len(views._timezones), 200)
+        self.assertLess(len(views._timezones), 2000)
+
+        # Check some timezones
+        tz = [tz for tz in views._timezones if tz[0] == 'Europe/Paris']
+        self.assertEqual(tz, [('Europe/Paris', '+01:00')])
+        tz = [tz for tz in views._timezones if tz[0] == 'America/New_York']
+        self.assertEqual(tz, [('America/New_York', '-05:00')])
